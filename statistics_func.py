0a0d0cf7bc5a863384eacbdd2532fd08c4a65053
LCMJ21 <jorge.lima.ne@gmail.com>
2022-03-27 20:55:06 +0100

    round

--- statistics_func.py
+++ statistics_func.py
@@ -7,19 +7,19 @@ def apply_func(func, numbers):
     result = 0
 
     if func_lower == 'sum':
-        result = sum(numbers)
+        result = round(sum(numbers), 1)
     elif func_lower == 'dp':
-        result = statistics.stdev(numbers)
+        result = round(statistics.stdev(numbers))
     elif func_lower == 'mode':
-        result = statistics.mode(numbers)
+        result = round(statistics.mode(numbers))
     elif func_lower == 'median':
-        result = statistics.median(numbers)
+        result = round(statistics.median(numbers))
     elif func_lower == 'max':
         result = max(numbers)
     elif func_lower == 'min':
         result = min(numbers)
     elif func_lower == 'mean':
-        result = statistics.mean(numbers)
+        result = round(statistics.mean(numbers))
     elif func_lower == 'grades':
         result = numbers
     return result
